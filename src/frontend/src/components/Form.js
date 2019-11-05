bd90e84c016bace7f99ad458a34fb886a3a28379
lindbost <linda.bostrom@tele2.com>
2019-11-05 15:45:32 +0100

    post form to backend

--- src/frontend/src/components/Form.js
+++ src/frontend/src/components/Form.js
@@ -1,27 +1,34 @@
 import React from 'react';
+import logo from '/Users/linda/Documents/code/game/src/frontend/src/Tele2_Logo_White.svg';
+import axios from 'axios';
 
 
     const Form = () => {
 
-       const handleSubmit = (event) => {
+       const handleSubmit = async(event) => {
             event.preventDefault();
             const name = event.target.name.value;
             const email = event.target.email.value;
+
             console.log(name);
             const data = {
                 name: name,
                 email: email
             }
             
-            fetch('/player', {
-              method: 'POST',
-              body: data,
-              headers: { 'Content-Type': 'application/json' } 
-            });
+            const res = await axios.post(
+                '/player',
+                { name: name,
+                  email: email
+                },
+                { headers: { 'Content-Type': 'application/json' } }
+            )
+            console.log(res);
           }
 
     return(
         <form className="form" onSubmit={handleSubmit}>
+            <img className="logo" src={logo} alt="“website logo”" />
             <div className="info">Do you want to learn more about tele2</div>
             <input placeholder="Name" type="text" label="Complete Name:" name="name"/>
             <input placeholder="Email" type="email" label="Email:" name="email"/>
@@ -33,3 +40,13 @@ import React from 'react';
 }
 
 export default Form;
+
+
+
+/*
+fetch('/player', {
+              method: 'POST',
+              body: data,
+              headers: { 'Content-Type': 'application/json' } 
+            });
+*/
