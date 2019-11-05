2d20a3b4553672e979ff2a637c9e23632fcc1486
lindbost <linda.bostrom@tele2.com>
2019-11-05 16:12:31 +0100

    update submit

--- src/frontend/src/components/Form.js
+++ src/frontend/src/components/Form.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '/Users/linda/Documents/code/game/src/frontend/src/Tele2_Logo_White.svg';
 import axios from 'axios';
 
 
     const Form = () => {
 
+        const [subForm, setSubForm] = useState(false);
+
        const handleSubmit = async(event) => {
             event.preventDefault();
             const name = event.target.name.value;
             const email = event.target.email.value;
 
-            console.log(name);
             const data = {
                 name: name,
                 email: email
@@ -26,27 +27,29 @@ import axios from 'axios';
             console.log(res);
           }
 
+          const doneSubmit = () => {
+            
+            console.log("done");
+            setSubForm(true);
+          }
+
     return(
+        <div>
+        {!subForm &&
         <form className="form" onSubmit={handleSubmit}>
             <img className="logo" src={logo} alt="“website logo”" />
             <div className="info">Do you want to learn more about tele2</div>
             <input placeholder="Name" type="text" label="Complete Name:" name="name"/>
             <input placeholder="Email" type="email" label="Email:" name="email"/>
-            <button className="submitBtn">
+            <button className="submitBtn" onClick={doneSubmit}>
                 SUBMIT
             </button>
         </form>
+        }
+        {subForm && <div>tack</div> }
+        <img className="logo" src={logo} alt="“website logo”" />
+        </div>
     );
 }
 
 export default Form;
-
-
-
-/*
-fetch('/player', {
-              method: 'POST',
-              body: data,
-              headers: { 'Content-Type': 'application/json' } 
-            });
-*/
