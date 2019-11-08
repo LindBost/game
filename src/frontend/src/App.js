1adfcf02ed37868a15903fa9f5a7f6ec5834a927
lindbost <linda.bostrom@tele2.com>
2019-11-08 11:22:48 +0100

    animations

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,31 @@
-import React, {useState} from 'react';
+import React, {useState, Fragment }from 'react';
 import './App.css';
 import logo from './Tele2_Logo_White.svg'
 import Quiz from "./components/Quiz";
-
+import posed from 'react-pose';
+import { linear } from '@popmotion/easing';
+import { statement } from '@babel/template';
 
 function App() {
+
+  const Container = posed.p({
+    attention: {
+      scale: 1.2,
+      transition: {
+        type: 'spring',
+        stiffness: 25,
+        damping: 0
+      }
+    }
+  });
+  
+  const Example = () => (
+    <Container className="container" initialPose="none" pose="attention">
+      <p className="box">
+      GET 2 KNOW TELE2 
+      </p>
+    </Container>
+  );
 
   const [gameStarted, setGameStarted] = useState(false)
 
@@ -19,7 +40,16 @@ function App() {
     <div className="App">
       <div className="headerContainer">
       <img src={logo} alt="“website logo”" className="logo"/>
+      {gameStarted &&
+      <div>
         <header className="header">GET 2 KNOW TELE2</header>
+      </div>
+      }
+      {!gameStarted &&
+      <div>
+        <Example className="headerAnimation"/>
+      </div>
+      }
       </div>
       {gameStarted &&
       <div>
